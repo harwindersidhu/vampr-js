f422f39b16f243bfefb92005922a4e013258353c
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-06-14 05:03:54 -0600

    New requirements

--- vampire.js
+++ vampire.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -38,6 +37,51 @@ class Vampire {
     return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
+  /** Tree traversal methods **/
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+
+    for (const child of this.offspring) {
+      let search = child.vampireWithName(name);
+      if (search) {
+        return search;
+      }
+    }
+    return null;
+    
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let vampSum = 0;
+
+    for (let vamp of this.offspring) {
+      vampSum += vamp.totalDescendents + 1;
+    }
+
+    return vampSum;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let resultingArray = [];
+
+    if (this.yearConverted > 1980) {
+      resultingArray.push(this);
+    }
+
+    for (const vamp of this.offspring) {
+      let array = vamp.allMillennialVampires;
+      resultingArray = resultingArray.concat(array);
+    }
+
+    return resultingArray;
+  }
+
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -73,66 +117,3 @@ let seniorsOfVampire = (vamp) => {
 
 module.exports = Vampire;
 
-=======
-class Vampire {
-  constructor(name, yearConverted) {
-    this.name = name;
-    this.yearConverted = yearConverted;
-    this.offspring = [];
-    this.creator = null;
-  }
-
-  /** Simple tree methods **/
-
-  // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
-
-  }
-
-  // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
-
-  }
-
-  // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-
-  }
-
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
-  }
-
-  /** Tree traversal methods **/
-
-  // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {
-    
-  }
-
-  // Returns the total number of vampires that exist
-  get totalDescendents() {
-    
-  }
-
-  // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
-  }
-
-  /** Stretch **/
-
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
-}
-
-module.exports = Vampire;
-
->>>>>>> origin/traversal
